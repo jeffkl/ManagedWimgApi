a898a151bac32d08c76f228886fd7c1051e810bb
Jeff Kluge <josemesona@hotmail.com>
2016-07-27 07:45:23 -0700

    Adding CBT as a submodule and updating projects to use it

--- src/Microsoft.Wim.Tests/Properties/AssemblyInfo.cs
+++ src/Microsoft.Wim.Tests/Properties/AssemblyInfo.cs
@@ -2,27 +2,4 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Microsoft.Wim.Tests")]
-
 [assembly: AssemblyDescription("Managed WimgApi Unit Tests")]
-
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
-
-[assembly: AssemblyCompany("Jeff Kluge")]
-
-[assembly: AssemblyProduct("Microsoft.WimgApi")]
-
-[assembly: AssemblyCopyright("Copyright ©  2013")]
-
-[assembly: AssemblyTrademark("")]
-
-[assembly: AssemblyCulture("")]
-
-[assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyFileVersion("1.0.0.0")]
-
-[assembly: ComVisible(false)]
