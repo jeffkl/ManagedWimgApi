fc4ea07cf26729ead47f4754af76013fd7e04a29
josemesona <josemesona@hotmail.com>
2016-11-05 15:26:53 -0700

    Switch to Nerdbank.GitVersioning

--- src/Microsoft.Wim.Tests/Properties/AssemblyInfo.cs
+++ src/Microsoft.Wim.Tests/Properties/AssemblyInfo.cs
@@ -1,5 +1,10 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Microsoft.Wim.Tests")]
 [assembly: AssemblyDescription("Managed WimgApi Unit Tests")]
+[assembly: AssemblyProduct("Microsoft.WimgApi")]
+[assembly: AssemblyCopyright("Copyright © 2013")]
+[assembly: CLSCompliant(false)]
+[assembly: ComVisible(false)]
